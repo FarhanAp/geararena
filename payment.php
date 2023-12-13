92ea9b760926262a22702bd9fd7e4403129223ce
Faniyalno <105857414+Faniyalno@users.noreply.github.com>
2023-12-13 15:19:21 +0800

    half baked payment method
    payment method form now can send array inside array

--- payment.php
+++ payment.php
@@ -1,16 +1,40 @@
 <?php 
     require 'function.php';
 
-    if (isset($_POST["checkout"])) {
+    $idpro = array();
+    $pro_name = array();
+    $quantity = array();
+    $total;
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["checkout"])) {
+        $total = $_POST["total"];
+        echo"$total";
+        echo"<br>";
+        echo "<pre>";
+        print_r($_SESSION['cart']);
+        echo "</pre>";
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["payment"])) {
         echo "<pre>";
         print_r($_POST);
         echo "</pre>";
-        echo"<br>";
-        echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
-
     }
+
+    foreach ($_SESSION['cart'] as $key => $value) {
+        array_push($idpro, $value['idpro']);
+        array_push($pro_name, $value['product']);
+        array_push($quantity, $value['quantity']);
+    }
+
+    echo "<pre>";
+    print_r($_SESSION['username']);
+    echo "</pre>";
+
+    print_r($idpro);
+    echo"<br>";
+    print_r($pro_name);
+    echo"<br>";
+    print_r($quantity);
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +44,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    
     <link rel="stylesheet" href="payment.css">
 
 </head>
@@ -28,9 +51,8 @@
 
 <div class="container">
 
-<section id="header">
+    <section id="header">
         <a href="#"><img src="images/GEAR_ARENA_v2_80x80.png" class="logo"alt=""></a>
-
         <div>
             <ul id="navbar">
                 <li><a href="marketplace.php">Home</a></li>
@@ -44,38 +66,34 @@
     </section>
 
 
-    <form action="">
+    <form action="payment.php" method="post">
         <div class="row">
-
             <div class="col">
-
                 <h3 class="title">billing address</h3>
 
                 <div class="inputBox">
-                    <span>full name :</span>
-                    <input type="text" placeholder="farhan ap">
+                    <span>username :</span>
+                    <input type="text" name="name" placeholder="Jane Doe" value="<?= $_SESSION['username'] ?>">
                 </div>
-                <div class="inputBox">
-                    <span>email :</span>
-                    <input type="email" placeholder="example@example.com">
-                </div>
+
                 <div class="inputBox">
                     <span>address :</span>
-                    <input type="text" placeholder="No - Street">
+                    <input type="text" name="address" placeholder="No - Street">
                 </div>
+
                 <div class="inputBox">
                     <span>city :</span>
-                    <input type="text" placeholder="Subang Jaya">
+                    <input type="text" name="city" placeholder="Subang Jaya">
                 </div>
 
                 <div class="flex">
                     <div class="inputBox">
                         <span>state :</span>
-                        <input type="text" placeholder="Selangor">
+                        <input type="text" name="state" placeholder="Selangor">
                     </div>
                     <div class="inputBox">
                         <span>post code :</span>
-                        <input type="text" placeholder="47500">
+                        <input type="text" name="postCode" placeholder="47500">
                     </div>
                 </div>
 
@@ -91,34 +109,36 @@
                 </div>
                 <div class="inputBox">
                     <span>name on card :</span>
-                    <input type="text" placeholder="mr. farhan ap">
+                    <input type="text" name="legalName" placeholder="mr. farhan ap">
                 </div>
                 <div class="inputBox">
                     <span>credit card number :</span>
-                    <input type="number" placeholder="1111-2222-3333-4444">
+                    <input type="number" name="ccNumber" placeholder="1111-2222-3333-4444">
                 </div>
                 <div class="inputBox">
-                    <span>exp month :</span>
-                    <input type="text" placeholder="january">
+                    <span>expired month :</span>
+                    <input type="text" name="expmonth" placeholder="january">
                 </div>
 
                 <div class="flex">
                     <div class="inputBox">
-                        <span>exp year :</span>
-                        <input type="number" placeholder="2022">
+                        <span>expired year :</span>
+                        <input type="number" name="expYear" placeholder="2022">
                     </div>
                     <div class="inputBox">
                         <span>CVV :</span>
-                        <input type="text" placeholder="1234">
+                        <input type="text" name="cvv" placeholder="1234">
                     </div>
                 </div>
 
             </div>
-    
         </div>
-
-        <input type="submit" value="proceed to payment" class="submit-btn">
-
+        <?php for ($i=0; $i < count($pro_name); $i++) { ?>
+            <input type="hidden" name="idpro[]" value="<?=$idpro[$i]?>">
+            <input type="hidden" name="product[]" value="<?=$pro_name[$i]?>">
+    
+        <?php } ?>
+        <button type="submit" name="payment" class="submit-btn">proceed to payment</button>
     </form>
 
 </div>    
